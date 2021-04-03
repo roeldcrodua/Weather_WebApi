5a1f46d2b813d3a2b61892fdf223ac647a3d32af
Roel Crodua <roelcrodua@yahoo.com>
2021-04-03 15:34:59 -0400

    Modify the distance value to have only 2 decimal value. Added some jsonproperty name.

--- Weather_WebApi/Models/Place/NearbyPlaces.cs
+++ Weather_WebApi/Models/Place/NearbyPlaces.cs
@@ -8,10 +8,21 @@ namespace Weather_WebApi.Models.Place
 {
     public class NearbyPlaces
     {
-        public double Distance { get; set; }
+        private double distance;
+        public double Distance
+        {
+            get { return distance; }
+            set { distance = Math.Round(value, 2);; }
+        }
+
+        [JsonPropertyName("place name")]
         public string PlaceName { get; set; }
+
+        [JsonPropertyName("post code")]
         public string PostCode { get; set; }
         public string State { get; set; }
+
+        [JsonPropertyName("state abbreviation")]
         public string StateAbbreviation { get; set; }
     }
 }
