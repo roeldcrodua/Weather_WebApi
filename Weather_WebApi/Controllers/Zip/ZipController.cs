3fa1bf7b05693c371b8bc9d5f0e1c6f6acac13f5
Roel Crodua <roelcrodua@yahoo.com>
2021-04-03 15:33:44 -0400

    Added a method GetZipInfo to get the place details as well as its nearby places

---
+++ Weather_WebApi/Controllers/Zip/ZipController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Weather_WebApi.Models.Place;
+
+namespace Weather_WebApi.Controllers.Zip
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZipController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetZipInfo(string country, string zip)
+        {
+
+            ZipInfo zipInfo = null;
+            NearbyInfo nearInfo = null;
+            using (var client = new HttpClient())
+            {
+                var json1 = await client.GetStringAsync("http://api.zippopotam.us/" + country + "/"  + zip);;
+                zipInfo = JsonSerializer.Deserialize<ZipInfo>(json1, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                var json2 = await client.GetStringAsync("http://api.zippopotam.us/nearby/US/" + zip);
+                nearInfo = JsonSerializer.Deserialize<NearbyInfo>(json2, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                zipInfo.Nearby = nearInfo.Nearby;
+            }
+            return View(zipInfo);
+        }
+
+    }
+}
---
+++ Weather_WebApi/Models/Place/ZipInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+namespace Weather_WebApi.Models.Place
+{
+    public class ZipInfo
+    {
+        [JsonPropertyName("post code")]
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+
+        [JsonPropertyName("country abbreviation")]
+        public string CountryAbbreviation { get; set; }
+        public PlaceInfo[] Places { get; set; }
+
+        public double Latitude
+        {
+            get { return double.Parse(Places[0].Latitude); }
+        }
+        public double Longitude
+        {
+            get { return double.Parse(Places[0].Longitude); }
+        }
+
+        public string PlaceName { get { return Places[0].PlaceName; } }
+        public string State { get { return Places[0].State; } }
+        public string StateAbbreviation { get { return Places[0].StateAbbreviation; } }
+
+        public NearbyPlaces[] Nearby { get; set; }
+    }
+}
